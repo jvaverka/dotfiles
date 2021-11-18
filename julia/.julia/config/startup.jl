5dbeb1002799ce1855ed4adf6fbe69ca757d60d1
Jake Vaverka <jvaverka@protonmail.com>
2021-11-18 16:24:33 -0500

    feat(julia): configure `@edit` to use lunarvim

--- julia/.julia/config/startup.jl
+++ julia/.julia/config/startup.jl
@@ -1,3 +1,13 @@
-println("Starting up Jake's Julia env ❤")
-
 using OhMyREPL
+using InteractiveUtils
+
+colorscheme!("OneDark")
+
+ENV["JULIA_EDITOR"] = "lvim"
+
+InteractiveUtils.define_editor(
+    r"lvim", wait=true) do cmd, path, line
+    `$cmd +$line $path`
+end
+
+println("               ❤                  ")
