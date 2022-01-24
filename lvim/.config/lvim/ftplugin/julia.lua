530800402d181f3c43524af641219d27cd8ccde1
Jake Vaverka <jvaverka@protonmail.com>
2022-01-24 12:13:47 -0500

    fix(lvim): global vim options for julia filetype

--- lvim/.config/lvim/ftplugin/julia.lua
+++ lvim/.config/lvim/ftplugin/julia.lua
@@ -7,6 +7,6 @@ require('lspconfig').julials.setup{}
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
-vim.b.expandtab = true
-vim.b.textwidth = 92
-vim.w.colorcolumn = 93
+vim.bo.expandtab = true
+vim.bo.textwidth = 92
+vim.wo.colorcolumn = '93'
