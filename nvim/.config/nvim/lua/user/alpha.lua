380ad014e1e4b38ffa8a8d6f42e61cc666647810
Jacob Vaverka <jvaverka@protonmail.com>
2022-04-28 23:48:04 -0400

    style(nvim): fix indents

--- nvim/.config/nvim/lua/user/alpha.lua
+++ nvim/.config/nvim/lua/user/alpha.lua
@@ -10,10 +10,10 @@ local space = {
 }
 
 function TableConcat(t1,t2)
-   for i=1,#t2 do
-      t1[#t1+1] = t2[i]
-   end
-   return t1
+  for i=1,#t2 do
+    t1[#t1+1] = t2[i]
+  end
+  return t1
 end
 
 local fortune_spaced = TableConcat(require'alpha.fortune'(), space)
