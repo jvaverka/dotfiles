f17ce0e5819703fd7c18bb159c0e7e22b37f175d
jvaverka <jvaverka@protonmail.com>
2022-04-28 21:27:58 -0400

    feat(julia): small startup additions

--- julia/.julia/config/startup.jl
+++ julia/.julia/config/startup.jl
@@ -1,12 +1,16 @@
+using Revise
 using OhMyREPL
 using InteractiveUtils
 
-colorscheme!("OneDark")
-
 ENV["JULIA_EDITOR"] = "lvim"
+
+const STARTUP = joinpath(homedir(), ".julia", "config", "startup.jl")
 
 InteractiveUtils.define_editor(r"lvim", wait = true) do cmd, path, line
     `$cmd +$line $path`
 end
 
+rmrf(path::String) = rm(path, force=true, recursive=true)
+
+colorscheme!("OneDark")
 println(join(["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "], ">>-<<"))
